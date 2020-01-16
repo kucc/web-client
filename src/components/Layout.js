8db9582a84f3d38ec2c9ae5c0f3383de04ad5a8d
hanameee <hanameee@gmail.com>
2020-01-16 23:49:41 +0900

    initiate directory with next.js

---
+++ src/components/Layout.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Layout = props => <div>{props.children}</div>;
+
+export default Layout;
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import Layout from "../components/Layout.js";
+
+const Index = () => <Layout>This is content</Layout>;
+
+export default Index;
