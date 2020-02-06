207db22889c4ce485afce8590c5d150b812ccd3f
riverandeye <riverandeye@gmail.com>
2020-02-07 00:49:25 +0900

    <feat>: add footer

--- components/footer/styles.tsx
+++ components/footer/styles.tsx
@@ -1,45 +1,49 @@
 import styled from 'styled-components';
 
 export const Footer = styled.div`
-  height: 18rem;
-  margin-top: 10rem;
-  padding: 4rem 25rem 4rem 35rem;
-  border-top: 1px solid #707070;
+  width: 136.6rem;
+  height: 8rem;
+
+  margin-top: 4rem;
+
+  background-color: #666666;
 
   display: flex;
-  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Container = styled.div`
+  width: 90.4rem;
+  display: flex;
+
+  align-items: center;
   justify-content: space-between;
 `;
 
-export const Messages = styled.div`
-  width: 71rem;
+export const DescriptionContainer = styled.div`
+  height: 4.7rem;
 
   display: flex;
   flex-direction: column;
+
+  align-items: center;
   justify-content: space-between;
 `;
 
-export const MessagePair = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
+export const DescriptionLineContainer = styled.div`
+  font-size: 1.4rem;
+  color: white;
 `;
 
-export const MessageKey = styled.div`
-  width: 19rem;
-  height: 3.5rem;
-  border: 1px solid #707070;
+export const LogoContainer = styled.div``;
+
+export const LogoCat = styled.img`
+  width: 4.7rem;
+  height: 4.7rem;
 `;
 
-export const MessageValue = styled.div`
-  width: 50rem;
-  height: 3.5rem;
-
-  border: 1px solid #707070;
-`;
-
-export const Logos = styled.div`
-  width: 22rem;
-
-  border: 1px solid #707070;
+export const LogoWord = styled.img`
+  width: 9.4rem;
+  height: 4.7rem;
 `;
