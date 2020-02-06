e01e1ad13544ed6ac58f94c546a1390094d5bbc6
riverandeye <riverandeye@gmail.com>
2020-02-07 00:48:47 +0900

    <feat>: add width in global styles
    컨텐츠가 136.6rem을 벗어나는 것을 방지하기 위해 설정

--- components/global-styles/index.tsx
+++ components/global-styles/index.tsx
@@ -18,13 +18,19 @@ const GlobalStyles = createGlobalStyle`
     background-color: white;
     /* user-select : none; 드래그 못하게 하기 */
     margin: 0;
-    
+    display :flex;
+    align-items : center;
+    justify-content : center;
   }::-webkit-scrollbar { /* 스크롤바 제거 */
     display: none;
   }
 
   #__next {
+    width : 136.6rem;
     height: 100%;
+    display : flex;
+    flex-direction : column;
+    align-items : center;
   }
 
   button {
