9fc7872ada24f30b148d9d5c245d24c131fcf2ae
riverandeye <riverandeye@gmail.com>
2020-02-12 22:20:06 +0900

    <feat>: add UserProvider and token deserialize logic on getinitialprops

--- views/App/index.tsx
+++ views/App/index.tsx
@@ -1,18 +1,65 @@
-import App from 'next/app';
 import React from 'react';
+import jwt from 'jsonwebtoken';
+import Cookies from 'universal-cookie';
+import App, { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
+
+import { UserProvider } from '../../context/user';
 
 const theme = {
   colors: {},
 };
 
-export default class MyApp extends App {
+class MyApp extends App<AppProps> {
+  public static async getInitialProps(appContext) {
+    const { pathname, req } = appContext.ctx;
+
+    const props = initProps(pathname);
+
+    if (req) {
+      try {
+        const cookies = new Cookies(req.headers.cookie);
+
+        const accessToken = cookies.get(process.env.JWT_ACCESS_TOKEN_KEY);
+
+        if (!accessToken) {
+          throw new Error();
+        }
+
+        const user = jwt.sign(accessToken, process.env.JWT_SECRET);
+
+        props.user = user;
+        appContext.ctx.isLoggedIn = true;
+      } catch (err) {
+        appContext.ctx.isLoggedIn = false;
+      }
+    }
+
+    const appProps = await App.getInitialProps(appContext);
+
+    return {
+      ...appProps,
+      ...props,
+    };
+  }
+
   render() {
-    const { Component, pageProps } = this.props;
+    const { Component, pageProps, user } = this.props;
     return (
       <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
+        <UserProvider user={user}>
+          <Component {...pageProps} />
+        </UserProvider>
       </ThemeProvider>
     );
   }
 }
+
+function initProps(pathname: string): AppProps {
+  return {
+    pathname,
+    user: null,
+  };
+}
+
+export default MyApp;
