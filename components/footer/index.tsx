e6f3a59e65e2cdb206971bd5612b354f6c6eaeae
hanameee <hanameee@gmail.com>
2020-02-12 16:39:43 +0900

    <refactor> Implement grid system to footer

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,22 +1,32 @@
 import * as S from './styles';
-
+import { Grid, Row, Col } from '../../components/grid/styles';
 const Footer: React.FC = () => {
   return (
     <S.Footer>
-      <S.Container>
-        <S.DescriptionContainer>
-          <S.DescriptionLineContainer>
-            이부분 너무 어려워
-          </S.DescriptionLineContainer>
-          <S.DescriptionLineContainer>
-            디자인 고민좀 해봅시다
-          </S.DescriptionLineContainer>
-        </S.DescriptionContainer>
-        <S.LogoContainer>
-          <S.LogoCat src="images/Footer-Cat-Black-Big.png" />
-          <S.LogoWord src="images/Logo-Word-White-Big.png" />
-        </S.LogoContainer>
-      </S.Container>
+      <S.FooterContainer>
+        <Grid>
+          <Row height="8rem">
+            <Col span={6} offset={2}>
+              <S.DescriptionContainer>
+                {/* prettier-ignore */}
+                <S.DescriptionLineContainer>
+                  <span style={{marginRight:"1rem", fontWeight:"bold"}}>연락처</span><span style={{marginRight:"4rem"}}>010-0000-0000</span>
+                  <span style={{marginRight:"1rem", fontWeight:"bold"}}>주소</span><span>서울특별시 성북구 안암동5가 1-13, 314호
+                  </span>
+                </S.DescriptionLineContainer>
+                <S.DescriptionLineContainer>
+                  Made with ❤️ of KUCC Members :)
+                </S.DescriptionLineContainer>
+              </S.DescriptionContainer>
+            </Col>
+            <Col span={2}>
+              <S.LogoContainer>
+                <S.LogoImg src="images/main_logo.png" alt="FooterLogo" />
+              </S.LogoContainer>
+            </Col>
+          </Row>
+        </Grid>
+      </S.FooterContainer>
     </S.Footer>
   );
 };
--- components/footer/styles.tsx
+++ components/footer/styles.tsx
@@ -3,49 +3,46 @@ import styled from 'styled-components';
 export const Footer = styled.div`
   width: 100%;
   margin-top: 4rem;
-
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
-export const Container = styled.div`
-  width: 100%;
-  padding: 0rem 23rem;
-
-  max-width: 136.6rem;
+export const FooterContainer = styled.div`
+  width: 128rem;
+  min-width: 96rem;
   height: 8rem;
-
   display: flex;
   justify-content: space-between;
   align-items: center;
-
   background-color: #666666;
 `;
 
 export const DescriptionContainer = styled.div`
-  height: 4.7rem;
-
   display: flex;
   flex-direction: column;
-
   align-items: center;
   justify-content: space-between;
 `;
 
 export const DescriptionLineContainer = styled.div`
-  font-size: 1.4rem;
+  width: 100%;
+  font-size: 1.2rem;
   color: white;
+  margin-bottom: 1.2rem;
+  &:last-child {
+    margin-bottom: 0;
+  }
 `;
 
-export const LogoContainer = styled.div``;
-
-export const LogoCat = styled.img`
-  width: 4.7rem;
-  height: 4.7rem;
+export const LogoContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
 `;
 
-export const LogoWord = styled.img`
-  width: 9.4rem;
-  height: 4.7rem;
+export const LogoImg = styled.img`
+  height: 4rem;
 `;
