eed555789e4f2fa9499d47406574343a9d3f4818
riverandeye <riverandeye@gmail.com>
2020-02-12 23:29:58 +0900

    <feat>: change header when logged-in

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,18 +1,23 @@
 import Link from 'next/link';
 import * as S from './styles';
 
+import { useUser } from '../../context/user';
 import { Grid, Row, Col } from '../../components/grid/styles';
 
 const Header: React.FC = () => {
+  const user = useUser();
+
   return (
     <S.Header>
       <S.HeaderContainer>
         <Grid>
           <Row height="6rem">
             <Col span={2}>
-              <S.LogoContainer>
-                <S.LogoImg src="images/Main-Logo.png" alt="HeaderLogo" />
-              </S.LogoContainer>
+              <Link href="/">
+                <S.LogoContainer>
+                  <S.LogoImg src="images/Main-Logo.png" alt="HeaderLogo" />
+                </S.LogoContainer>
+              </Link>
             </Col>
             <Col span={6} offset={1}>
               <S.MenuContainer>
@@ -26,17 +31,23 @@ const Header: React.FC = () => {
               </S.MenuContainer>
             </Col>
             <Col span={2} offset={1}>
-              <S.ProfileContainer>
-                <S.ProfileIcon>
-                  <div className="circle" />
-                </S.ProfileIcon>
-                <S.ProfileIcon>
-                  <div className="circle" />
-                </S.ProfileIcon>
-                <S.ProfileAvatar>
-                  <img src="images/Avatar-Black-Dummy.png" />
-                </S.ProfileAvatar>
-              </S.ProfileContainer>
+              {user ? (
+                <S.ProfileContainer>
+                  <S.ProfileIcon>
+                    <div className="circle" />
+                  </S.ProfileIcon>
+                  <S.ProfileIcon>
+                    <div className="circle" />
+                  </S.ProfileIcon>
+                  <S.ProfileAvatar>
+                    <img src="images/Avatar-Black-Dummy.png" />
+                  </S.ProfileAvatar>
+                </S.ProfileContainer>
+              ) : (
+                <Link href="login">
+                  <S.LoginButton>로그인</S.LoginButton>
+                </Link>
+              )}
             </Col>
           </Row>
         </Grid>
