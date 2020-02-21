b553886d1e44ddb5d249b42339fc3fd3dcc8e5fd
r4bb1t <danettofficial@gmail.com>
2020-02-21 14:53:00 +0900

    <feat> add landing page

--- views/main/index.tsx
+++ views/main/index.tsx
@@ -1,12 +1,104 @@
 import { NextPage } from 'next';
 
 import * as S from './styles';
+import { Grid, Row, Col } from '../../components/grid/styles';
 import Layout from '../../components/layout';
 
 const Main: NextPage = () => (
   <Layout>
     <S.Main>
-      <S.Index></S.Index>
+      <Grid>
+        <S.Section1>
+          <Row>
+            <Col>
+              <Row>
+                <S.MiniFeatureContainer>
+                  <S.MiniFeature>Assemble</S.MiniFeature>
+                  <S.MiniFeature>Scrum</S.MiniFeature>
+                  <S.MiniFeature>Develop</S.MiniFeature>
+                  <S.MiniFeature>Further</S.MiniFeature>
+                </S.MiniFeatureContainer>
+              </Row>
+              <Row>
+                <S.TitleMessage>
+                  We Code;
+                  <br />
+                  Together
+                </S.TitleMessage>
+              </Row>
+              <Row>
+                <S.TitleContent>
+                  고려대학교 중앙 컴퓨터 동아리 KUCC는
+                  <br />
+                  컴퓨터에 관심이 있고, 더 알고 싶고,
+                  <br />
+                  다른 이들과 나누고 싶어하는 사람들이 모인 동아리입니다!(예시)
+                </S.TitleContent>
+              </Row>
+            </Col>
+            <Col>
+              <S.ImgContainer>
+                <S.Image src="https://cdn.pixabay.com/photo/2015/07/17/22/43/student-849822_960_720.jpg" />
+              </S.ImgContainer>
+            </Col>
+          </Row>
+        </S.Section1>
+        <S.RowSpace />
+        <S.Section2>
+          <Row>
+            <Col>
+              <Grid>
+                <Row>
+                  <Col>
+                    <S.ImageGrid src="https://cdn.pixabay.com/photo/2015/07/17/22/43/student-849822_960_720.jpg" />
+                  </Col>
+                  <Col>
+                    <S.ImageGrid src="https://cdn.pixabay.com/photo/2015/07/17/22/43/student-849822_960_720.jpg" />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <S.ImageGrid src="https://cdn.pixabay.com/photo/2015/07/17/22/43/student-849822_960_720.jpg" />
+                  </Col>
+                  <Col>
+                    <S.ImageGrid src="https://cdn.pixabay.com/photo/2015/07/17/22/43/student-849822_960_720.jpg" />
+                  </Col>
+                </Row>
+              </Grid>
+            </Col>
+            <Col>
+              <Row>
+                <S.Message>Footage</S.Message>
+              </Row>
+              <Row>
+                <S.Content>
+                  KUCC는 초심자 분들이
+                  <br /> 프로그래밍에 진입하는 문턱을 <br />
+                  낮추기 위해 노력합니다.(예시)
+                </S.Content>
+              </Row>
+            </Col>
+          </Row>
+        </S.Section2>
+        <S.RowSpace />
+        <S.Section3>
+          <Row>
+            <Col>
+              <Row>
+                <S.Message>Footage</S.Message>
+              </Row>
+              <Row>
+                <S.Content>
+                  KUCC는 초심자 분들이
+                  <br /> 프로그래밍에 진입하는 문턱을 <br />
+                  낮추기 위해 노력합니다.(예시)
+                </S.Content>
+              </Row>
+            </Col>
+            <Col></Col>
+          </Row>
+        </S.Section3>
+      </Grid>
     </S.Main>
   </Layout>
 );
--- views/main/styles.tsx
+++ views/main/styles.tsx
@@ -6,13 +6,86 @@ export const Main = styled.div`
   align-items: center;
 `;
 
-export const Index = styled.div`
-  /* page 테스트 */
-  width: 80rem;
-  font-size: 2rem;
-  text-align: center;
+export const Section1 = styled.div``;
+
+export const Section2 = styled.div``;
+
+export const Section3 = styled.div`
+  background: #f3f3f3;
 `;
 
-export const Description = styled.div`
-  font-size: 1.5rem;
+export const Section4 = styled.div``;
+
+export const MiniFeature = styled.div`
+  border: 1px solid #1a1311;
+  border-radius: 1rem;
+  padding: 0.1rem 1.2rem;
+  font-weight: 700;
+  color: black;
+  letter-spacing: 0.1rem;
+`;
+
+export const MiniFeatureContainer = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  padding-left: 5rem;
+  padding-right: 25rem;
+`;
+
+export const ImgContainer = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+export const TitleMessage = styled.div`
+  color: #c93333;
+  font-size: 5rem;
+  font-weight: 800;
+  letter-spacing: 1.2rem;
+  margin-left: 5rem;
+  line-height: 7rem;
+`;
+
+export const TitleContent = styled.div`
+  font-size: 1.4rem;
+  font-weight: 700;
+  margin-left: 5rem;
+  margin-top: 1rem;
+  color: black;
+  line-height: 2.4rem;
+`;
+
+export const Image = styled.img`
+  box-shadow: 0px 5px 5px;
+  width: 100%;
+`;
+
+export const ImageGrid = styled.img`
+  width: 100%;
+  margin: 1rem;
+`;
+
+export const RowSpace = styled.div`
+  height: 20rem;
+`;
+
+export const Message = styled.div`
+  color: #c93333;
+  font-size: 5rem;
+  font-weight: 700;
+  letter-spacing: 1.2rem;
+  margin-left: 10rem;
+  line-height: 7rem;
+`;
+
+export const Content = styled.div`
+  font-size: 1.8rem;
+  font-weight: 400;
+  margin-left: 20rem;
+  margin-top: 1rem;
+  line-height: 2.5rem;
 `;
