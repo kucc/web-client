20273f3cbedebdf77bd0d7d2557945fbaff7f1d7
riverandeye <riverandeye@gmail.com>
2020-02-23 14:57:49 +0900

    <feat>: init schedule component

---
+++ components/schedule/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles';
+
+interface ScheduleProps {}
+
+const Schedule: React.FunctionComponent<ScheduleProps> = ({}) => {
+  return <S.Schedule></S.Schedule>;
+};
+
+export default Schedule;
---
+++ components/schedule/styles.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components/macro';
+
+export const Schedule = styled.div`
+  width: 100%;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  justify-content: space-between;
+`;
