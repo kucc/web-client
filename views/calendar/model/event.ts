9a04a87bf5a25151aecb36aef42d14e10f2083df
riverandeye <riverandeye@gmail.com>
2020-02-28 03:06:51 +0900

    <feat>: add constructor

--- views/calendar/model/event.ts
+++ views/calendar/model/event.ts
@@ -1,12 +1,23 @@
+import EventResponse from '../interface/event-response';
+
 export default class Event {
-  public constructor(
-    public readonly Id: number,
-    public readonly name: string,
-    public readonly userId: number,
-    public readonly description: string,
-    public readonly startAt: Date,
-    public readonly endAt: Date,
-    public readonly color: string,
-    public readonly place: string,
-  ) {}
+  public constructor(event: EventResponse) {
+    this.Id = event.Id;
+    this.name = event.name;
+    this.userId = event.userId;
+    this.description = event.description;
+    this.startAt = new Date(event.startAt);
+    this.endAt = new Date(event.endAt);
+    this.color = event.color;
+    this.place = event.place;
+  }
+
+  public readonly Id: number;
+  public readonly name: string;
+  public readonly userId: number;
+  public readonly description: string;
+  public readonly startAt: Date;
+  public readonly endAt: Date;
+  public readonly color: string;
+  public readonly place: string;
 }
