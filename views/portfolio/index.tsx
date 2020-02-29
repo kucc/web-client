3ff5e550a4cb0fa1cf7723b35fef6311383d53da
riverandeye <riverandeye@gmail.com>
2020-02-29 23:19:21 +0900

    <feat>: add portfolio page

---
+++ views/portfolio/index.tsx
@@ -0,0 +1,57 @@
+import { NextPage } from 'next';
+
+import * as S from './styles';
+import Layout from '../../components/layout';
+import { Grid, Row, Col } from '../../components/grid/styles';
+
+import PortfolioNavigation from '../../components/portfolio-navigation';
+
+const Portfolio: NextPage = () => {
+  return (
+    <Layout>
+      <S.Portfolio>
+        <Grid>
+          <Row>
+            <S.PortfolioContainer>
+              <Col span={2}>
+                <S.PortfolioNavBar>
+                  <PortfolioNavigation />
+                </S.PortfolioNavBar>
+              </Col>
+              <Col span={10}>
+                <S.PortfolioMenu>
+                  <S.PortfolioMenuTitle>포트폴리오</S.PortfolioMenuTitle>
+                  <S.PortfolioMenuHistory>
+                    Home > Portfolio
+                  </S.PortfolioMenuHistory>
+                </S.PortfolioMenu>
+                <S.PortfolioContent>
+                  <S.Slider bullets={false} animation="scaleOutAnimation">
+                    <S.SliderContentWrapper>
+                      <S.SliderContentBackground backgroundColor="#F7D9CD">
+                        인생
+                      </S.SliderContentBackground>
+                    </S.SliderContentWrapper>
+                    <S.SliderContentWrapper>
+                      f
+                      <S.SliderContentBackground backgroundColor="#C6F0FF">
+                        사람은 지쳐 쓰러질 때 까지 일해야 한다 - 박지성 -
+                      </S.SliderContentBackground>
+                    </S.SliderContentWrapper>
+                    <S.SliderContentWrapper>
+                      <S.SliderContentBackground backgroundColor="#ECECB0">
+                        이런느낌어때?
+                      </S.SliderContentBackground>
+                    </S.SliderContentWrapper>
+                  </S.Slider>
+                </S.PortfolioContent>
+              </Col>
+            </S.PortfolioContainer>
+          </Row>
+        </Grid>
+      </S.Portfolio>
+    </Layout>
+  );
+};
+
+export default Portfolio;
---
+++ views/portfolio/styles.tsx
@@ -0,0 +1,75 @@
+import styled from 'styled-components';
+import AwesomeSlider from 'react-awesome-slider';
+
+export const Portfolio = styled.div``;
+
+export const PortfolioContainer = styled.div`
+  width: 100%;
+  max-width: 136.6rem;
+  min-width: 96rem;
+  margin: 0 auto;
+  min-height: 45rem;
+`;
+
+export const PortfolioNavBar = styled.ul`
+  font-size: 1.8rem;
+  margin-top: 8rem;
+`;
+
+export const PortfolioMenu = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0 2rem;
+  margin: 2rem 0 3rem 0;
+`;
+
+export const PortfolioMenuTitle = styled.div`
+  font-size: 2.5rem;
+  font-weight: bold;
+`;
+
+export const PortfolioMenuHistory = styled.div`
+  font-size: 1.2rem;
+  margin-left: auto;
+  margin-right: 1rem;
+`;
+
+export const PortfolioContent = styled.div`
+  width: 100%;
+  height: 49rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  overflow: hidden;
+`;
+
+export const Slider = styled(AwesomeSlider)``;
+
+export const SliderContentWrapper = styled.div``;
+
+interface SliderContentProps {
+  backgroundImage?: string;
+  backgroundColor?: string;
+  color?: string;
+}
+
+export const SliderContentBackground = styled.div<SliderContentProps>`
+  max-width: 136.6rem;
+  min-width: 106rem;
+  height: 80rem;
+
+  display: flex;
+
+  justify-content: center;
+  align-items: center;
+
+  font-size: 2rem;
+  background-color: white;
+  color: ${props => (props.color ? props.color : 'black')};
+
+  background-color: ${props =>
+    props.backgroundColor ? props.backgroundColor : 'white'};
+`;
