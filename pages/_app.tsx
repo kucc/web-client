c8a0366996da3f66732d2d646a33f5e30702fb36
riverandeye <riverandeye@gmail.com>
2020-02-29 23:17:59 +0900

    <feat>: add global css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,11 @@
 import 'codemirror/lib/codemirror.css';
 import 'tui-editor/dist/tui-editor.min.css';
+import 'react-awesome-slider/dist/styles.css';
+// import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
+// import 'react-awesome-slider/dist/custom-animations/fall-animation.css';
+// import 'react-awesome-slider/dist/custom-animations/open-animation.css';
+// import 'react-awesome-slider/dist/custom-animations/fold-out-animation.css';
+import 'react-awesome-slider/dist/custom-animations/scale-out-animation.css';
 import 'tui-editor/dist/tui-editor-contents.min.css';
 
 export { default } from '../views/app';
