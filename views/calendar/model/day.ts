d3b859f38e712368a8f3d7cfe8488c113a793fa9
riverandeye <riverandeye@gmail.com>
2020-02-29 10:02:54 +0900

    <feat>: add day model

--- views/calendar/model/day.ts
+++ views/calendar/model/day.ts
@@ -1,8 +1,34 @@
 import Event from './event';
 
 export default class Day {
-  public day: number;
+  public constructor(day: Date, isThisMonth: boolean) {
+    this.day = day;
+    this.lastMoment = this.getLastMomentOfThisDay();
+    this.isThisMonth = isThisMonth;
+    this.isSunday = day.getDay() === 0;
+    this.isSaturday = day.getDay() === 6;
+    this.events = [];
+  }
+
+  public day: Date;
+  public lastMoment: Date;
   public events: Event[];
-  public isHoliday: boolean;
   public isSaturday: boolean;
+  public isSunday: boolean;
+  public isThisMonth: boolean;
+
+  public addEvent(event: Event) {
+    this.events.push(event);
+  }
+
+  public getLastMomentOfThisDay() {
+    return new Date(
+      this.day.getFullYear(),
+      this.day.getMonth(),
+      this.day.getDate(),
+      23,
+      59,
+      59,
+    );
+  }
 }
